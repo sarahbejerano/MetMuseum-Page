a69f71fce6aa66c4acd2b423b7e09b59ded82f56
sarahbejerano <bejeranosarah@gmail.com>
2021-11-13 02:14:09 +0000

    looking for the footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,11 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+// export const Footer = () => (
+
+// );
